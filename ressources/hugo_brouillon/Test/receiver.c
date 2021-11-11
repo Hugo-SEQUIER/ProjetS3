ca6d8fea3162fe5ba59c994cb08788d7206ea26c
sequierh <hugo.sequier@etu.umontpellier.fr>
2021-11-11 22:07:50 +0100

    Commentaires

--- ressources/hugo_brouillon/Test/receiver.c
+++ ressources/hugo_brouillon/Test/receiver.c
@@ -1,139 +1,133 @@
 #include "contiki.h"
-#include "lib/random.h"
-#include "sys/ctimer.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
 #include "net/uip.h"
-#include "net/uip-ds6.h"
-#include "net/uip-udp-packet.h"
-#include "sys/ctimer.h"
+#include "net/rpl/rpl.h"
+
+#include "net/netstack.h"
 #include "dev/button-sensor.h"
-#ifdef WITH_COMPOWER
-#include "powertrace.h"
-#endif
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "dev/leds.h"
-
-#define UDP_CLIENT_PORT 8765
-#define UDP_SERVER_PORT 5678
-
-#define UDP_EXAMPLE_ID  190
 
 #define DEBUG DEBUG_PRINT
 
 #include "net/uip-debug.h"
 
-#ifndef PERIOD
-#define PERIOD 60
-#endif
+#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 
-#define START_INTERVAL        (15 * CLOCK_SECOND)
-#define SEND_INTERVAL        (PERIOD * CLOCK_SECOND)
-#define SEND_TIME        (random_rand() % (SEND_INTERVAL))
-#define MAX_PAYLOAD_LEN        30
+#define UDP_CLIENT_PORT    8765
+#define UDP_SERVER_PORT    5678
 
+#define UDP_EXAMPLE_ID  190
 
-static struct uip_udp_conn *client_conn;
-static uip_ipaddr_t server_ipaddr;
+static struct uip_udp_conn *server_conn;
 
-/*---------------------------------------------------------------------------*/
-PROCESS(udp_client_process,
-"UDP client process");
-AUTOSTART_PROCESSES(&udp_client_process);
+PROCESS(udp_server_process,
+"UDP server process");
+AUTOSTART_PROCESSES(&udp_server_process);
 
 /*---------------------------------------------------------------------------*/
-static void tcpip_handler(void) {
-    char *str;
+/** Ici on gère la reception du message **/
+static void
+tcpip_handler(void) {
+    char *appdata;
 
     if (uip_newdata()) {
-        str = uip_appdata;
-        str[uip_datalen()] = '\0';
-        printf("J'ai reçu : '%s'\n", str);
+        /** On stock dans appdata le contenu du message **/
+        appdata = (char *) uip_appdata;
+        appdata[uip_datalen()] = '\0';
+        /** On affiche le message reçu **/
+        PRINTF("On m'a envoye '%s' de ", appdata);
+        leds_on(LEDS_BLUE);
+        /** On affiche l'expéditeur du message **/
+        PRINTF("%d",
+               UIP_IP_BUF->srcipaddr.u8[sizeof(UIP_IP_BUF->srcipaddr.u8) - 1]);
+        PRINTF("\n");
+        /** On envoie un accusé de reception **/
+        uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
+        uip_udp_packet_send(server_conn, "Message bien envoye !", sizeof("Message bien envoye !"));
+        uip_create_unspecified(&server_conn->ripaddr);
+
     }
 }
 
 /*---------------------------------------------------------------------------*/
-static void send_packet(void *ptr) {
-    char buf[MAX_PAYLOAD_LEN];
-
-    PRINTF("J'envoie à  %d 'Salut'\n", server_ipaddr.u8[sizeof(server_ipaddr.u8) - 1]);
-    leds_on(LEDS_RED);
-    sprintf(buf, "<3", 1);
-    uip_udp_packet_sendto(client_conn, buf, strlen(buf), &server_ipaddr, UIP_HTONS(UDP_SERVER_PORT));
-}
-
-/*---------------------------------------------------------------------------*/
-static void set_global_address(void) {
-    uip_ipaddr_t ipaddr;
-
-    uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-    uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-    uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
-
-/* The choice of server address determines its 6LoPAN header compression.
- * (Our address will be compressed Mode 3 since it is derived from our link-local address)
- * Obviously the choice made here must also be selected in udp-server.c.
- *
- * For correct Wireshark decoding using a sniffer, add the /64 prefix to the 6LowPAN protocol preferences,
- * e.g. set Context 0 to aaaa::.  At present Wireshark copies Context/128 and then overwrites it.
- * (Setting Context 0 to aaaa::1111:2222:3333:4444 will report a 16 bit compressed address of aaaa::1111:22ff:fe33:xxxx)
- *
- * Note the IPCMV6 checksum verification depends on the correct uncompressed addresses.
- */
-
-#if 0
-    /* Mode 1 - 64 bits inline */
-       uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
-#elif 1
-/* Mode 2 - 16 bits inline */
-    uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
-#else
-    /* Mode 3 - derived from server link-local (MAC) address */
-      uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a); //redbee-econotag
-#endif
-}
-
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(udp_client_process, ev, data
+PROCESS_THREAD(udp_server_process, ev, data
 )
 {
-static struct etimer periodic;
-static struct ctimer backoff_timer;
-#if WITH_COMPOWER
-static int print = 0;
-#endif
+uip_ipaddr_t ipaddr;
+struct uip_ds6_addr *root_if;
 
 PROCESS_BEGIN();
 
 PROCESS_PAUSE();
-SENSORS_ACTIVATE(button_sensor);
-set_global_address();
 
-/* new connection with remote host */
-client_conn = udp_new(NULL, UIP_HTONS(UDP_SERVER_PORT), NULL);
-if(client_conn == NULL) {
-PRINTF("Erreur on stop tout!\n");
+SENSORS_ACTIVATE(button_sensor);
+
+/** Ici on set l'adresse qui est la même que dans sender.c**/
+#if UIP_CONF_ROUTER
+/* Le choix de l'adresse du serveur détermine sa compression d'en-tête 6LoWPAN.
+ * Évidemment, le choix fait ici doit également être sélectionné dans udp-client.c.
+ *
+ * Pour un décodage Wireshark correct à l'aide d'un renifleur, ajoutez le préfixe /64 aux préférences du protocole 6LowPAN,
+ * par exemple. définissez Contexte 0 sur aaaa ::. À l'heure actuelle, Wireshark copie Context/128, puis l'écrase.
+ * (La définition du contexte 0 sur aaaa::1111:2222:3333:4444 signalera une adresse compressée 16 bits de aaaa::1111:22ff:fe33:xxxx)
+ * Remarque La vérification de la somme de contrôle IPCMV6 de Wireshark dépend des bonnes adresses non compressées.
+ */
+
+#if 0
+/* Mode 1 - 64 bits inline */
+   uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
+#elif 1
+/* Mode 2 - 16 bits inline */
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
+#else
+/* Mode 3 - derived from link local (MAC) address */
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+#endif
+/** Création du DooDag **/
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_MANUAL);
+  root_if = uip_ds6_addr_lookup(&ipaddr);
+  if(root_if != NULL) {
+    rpl_dag_t *dag;
+    dag = rpl_set_root(RPL_DEFAULT_INSTANCE,(uip_ip6addr_t *)&ipaddr);
+    uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    rpl_set_prefix(dag, &ipaddr, 64);
+    PRINTF("created a new RPL dag\n");
+  } else {
+    PRINTF("failed to create a new RPL DAG\n");
+  }
+#endif /* UIP_CONF_ROUTER */
+NETSTACK_MAC.off(1);
+
+/** On set la connection **/
+server_conn = udp_new(NULL, UIP_HTONS(UDP_CLIENT_PORT), NULL);
+if(server_conn == NULL) {
+PRINTF("No UDP connection available, exiting the process!\n");
 
 PROCESS_EXIT();
 
 }
-udp_bind(client_conn, UIP_HTONS(
-UDP_CLIENT_PORT));
+/** On lie la connection au local port **/
+udp_bind(server_conn, UIP_HTONS(
+UDP_SERVER_PORT));
 
-etimer_set(&periodic, SEND_INTERVAL);
 while(1) {
+/** Le process est en attente **/
 PROCESS_YIELD();
-
+/** Si le process reçoit un event tcp **/
 if(ev == tcpip_event) {
 tcpip_handler();
 
-}
-
-if(etimer_expired(&periodic) && data== &button_sensor) {
-etimer_reset(&periodic);
-ctimer_set(&backoff_timer, SEND_TIME, send_packet, NULL);
-
-
+/** Si on clique sur le bouton du mote **/
+} else if (ev == sensors_event && data == &button_sensor) {
+PRINTF("Initiaing global repair\n");
+/** On refait le DoDaG**/
+rpl_repair_root(RPL_DEFAULT_INSTANCE);
 }
 }
 
@@ -141,4 +135,3 @@ PROCESS_END();
 
 }
 /*---------------------------------------------------------------------------*/
-
--- ressources/hugo_brouillon/Test/sender.c
+++ ressources/hugo_brouillon/Test/sender.c
@@ -42,27 +42,30 @@ PROCESS(udp_client_process,
 AUTOSTART_PROCESSES(&udp_client_process);
 
 /*---------------------------------------------------------------------------*/
+/** Ici on gère la reception du message **/
 static void tcpip_handler(void) {
     char *str;
-
     if (uip_newdata()) {
+        /** On stock le message et on l'affiche **/
         str = uip_appdata;
         str[uip_datalen()] = '\0';
-        printf("J'ai reçu : '%s'\n", str);
+        printf("%s\n", str);
     }
 }
 
 /*---------------------------------------------------------------------------*/
+/** Ici on gère l'envoie du message **/
 static void send_packet(void *ptr) {
     char buf[MAX_PAYLOAD_LEN];
 
-    PRINTF("J'envoie à  %d 'Salut'\n", server_ipaddr.u8[sizeof(server_ipaddr.u8) - 1]);
+    PRINTF("J'envoie <3 à '%d' \n", &server_ipaddr);
     leds_on(LEDS_RED);
     sprintf(buf, "<3", 1);
     uip_udp_packet_sendto(client_conn, buf, strlen(buf), &server_ipaddr, UIP_HTONS(UDP_SERVER_PORT));
 }
 
 /*---------------------------------------------------------------------------*/
+/** Ici on set l'adresse qui est la même que dans receiver.c**/
 static void set_global_address(void) {
     uip_ipaddr_t ipaddr;
 
@@ -70,15 +73,15 @@ static void set_global_address(void) {
     uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
     uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
 
-/* The choice of server address determines its 6LoPAN header compression.
- * (Our address will be compressed Mode 3 since it is derived from our link-local address)
- * Obviously the choice made here must also be selected in udp-server.c.
+/* Le choix de l'adresse du serveur détermine sa compression d'en-tête 6LoWPAN.
+ * (Notre adresse sera compressée en Mode 3 puisqu'elle est dérivée de notre adresse lien-local)
+ * Évidemment, le choix fait ici doit également être sélectionné dans udp-server.c.
  *
- * For correct Wireshark decoding using a sniffer, add the /64 prefix to the 6LowPAN protocol preferences,
- * e.g. set Context 0 to aaaa::.  At present Wireshark copies Context/128 and then overwrites it.
- * (Setting Context 0 to aaaa::1111:2222:3333:4444 will report a 16 bit compressed address of aaaa::1111:22ff:fe33:xxxx)
+ * Pour un décodage Wireshark correct à l'aide d'un renifleur, ajoutez le préfixe /64 aux préférences du protocole 6LowPAN,
+ * par exemple. définissez Contexte 0 sur aaaa ::. À l'heure actuelle, Wireshark copie Context/128, puis l'écrase.
+ * (La définition du contexte 0 sur aaaa::1111:2222:3333:4444 signalera une adresse compressée 16 bits de aaaa::1111:22ff:fe33:xxxx)
  *
- * Note the IPCMV6 checksum verification depends on the correct uncompressed addresses.
+ * Notez que la vérification de la somme de contrôle IPCMV6 dépend des bonnes adresses non compressées.
  */
 
 #if 0
@@ -110,6 +113,7 @@ SENSORS_ACTIVATE(button_sensor);
 set_global_address();
 
 /* new connection with remote host */
+/** On set la connection **/
 client_conn = udp_new(NULL, UIP_HTONS(UDP_SERVER_PORT), NULL);
 if(client_conn == NULL) {
 PRINTF("Erreur on stop tout!\n");
@@ -117,10 +121,13 @@ PRINTF("Erreur on stop tout!\n");
 PROCESS_EXIT();
 
 }
+/** On lie la connection au local port **/
 udp_bind(client_conn, UIP_HTONS(
 UDP_CLIENT_PORT));
 
-
+#if WITH_COMPOWER
+powertrace_sniff(POWERTRACE_ON);
+#endif
 
 etimer_set(&periodic, SEND_INTERVAL);
 while(1) {
@@ -135,7 +142,14 @@ if(etimer_expired(&periodic) && data== &button_sensor) {
 etimer_reset(&periodic);
 ctimer_set(&backoff_timer, SEND_TIME, send_packet, NULL);
 
-
+#if WITH_COMPOWER
+if (print == 0) {
+powertrace_print("#P");
+}
+if (++print == 3) {
+print = 0;
+}
+#endif
 
 }
 }
@@ -144,4 +158,3 @@ PROCESS_END();
 
 }
 /*---------------------------------------------------------------------------*/
-
